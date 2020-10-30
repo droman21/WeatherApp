731032a57619ded127027e4d0ac2228f6bd18386
Daniel Roman <dan.roman21@gmail.com>
2020-10-30 08:23:53 -0400

    Final styling changes

--- App.js
+++ App.js
@@ -1,85 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import * as Location from 'expo-location';
-import WeatherInfo from './components/WeatherInfo';
-import UnitsPicker from './components/UnitsPicker';
-import ReloadIcon from './components/ReloadIcon';
+import { StatusBar } from 'expo-status-bar'
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native'
+import * as Location from 'expo-location'
+import WeatherInfo from './components/WeatherInfo'
+import UnitsPicker from './components/UnitsPicker'
+import ReloadIcon from './components/ReloadIcon'
+import WeatherDetails from './components/WeatherDetails'
+import { colors } from './utils/index'
 
 
 const WEATHER_API_KEY = '8a6ab51b54067527f45a9711f78435c5'
 const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?'
-
 export default function App() {
-
   const [errorMessage, setErrorMessage] = useState(null)
   const [currentWeather, setCurrentWeather] = useState(null)
-  const [unitsSystem, setUnitsSystem] = useState('imperial')
+  const [unitsSystem, setUnitsSystem] = useState('metric')
 
   useEffect(() => {
-    load()
+      load()
   }, [unitsSystem])
 
   async function load() {
-    setCurrentWeather(null)
-    try {
-      let { status } = await Location.requestPermissionsAsync()
+      setCurrentWeather(null)
+      setErrorMessage(null)
+      try {
+          let { status } = await Location.requestPermissionsAsync()
 
-      if(status != 'granted') {
-        setErrorMessage('Access to Location is Needed to Run This App')
-        return 
+          if (status !== 'granted') {
+              setErrorMessage('Access to location is needed to run the app')
+              return
+          }
+          const location = await Location.getCurrentPositionAsync()
+
+          const { latitude, longitude } = location.coords
+
+          const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`
+
+          const response = await fetch(weatherUrl)
+
+          const result = await response.json()
+
+          if (response.ok) {
+              setCurrentWeather(result)
+          } else {
+              setErrorMessage(result.message)
+          }
+      } catch (error) {
+          setErrorMessage(error.message)
       }
-      const location = await Location.getCurrentPositionAsync()
-
-      const { latitude, longitude } = location.coords
-
-      const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`
-
-      const response = await fetch(weatherUrl)
-
-      const result = await response.json()
-
-      if(response.ok) {
-        setCurrentWeather(result)
-      } else {
-        setErrorMessage(result.message)
-      }
-
-    } catch (error) {}
   }
-  if(currentWeather) {
-    
-    return (
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-        <View style={styles.main}>
-          <UnitsPicker unitsSystem={unitsSystem} setUnitsSystem={setUnitsSystem} />
-          <ReloadIcon load={load} />
-          <WeatherInfo currentWeather={currentWeather} />
-        </View>
-      </View>
-    )
-    } else {
+  if (currentWeather) {
       return (
-        <View style={styles.container}>
-          <Text>{errorMessage}</Text>
-          <StatusBar style="auto" />
-        </View>
+          <View style={styles.container}>
+              <StatusBar style="auto" />
+              <View style={styles.main}>
+                  <UnitsPicker unitsSystem={unitsSystem} setUnitsSystem={setUnitsSystem} />
+                  <ReloadIcon load={load} />
+                  <WeatherInfo currentWeather={currentWeather} />
+              </View>
+              <WeatherDetails currentWeather={currentWeather} unitsSystem={unitsSystem} />
+          </View>
       )
-
+  } else if (errorMessage) {
+      return (
+          <View style={styles.container}>
+              <ReloadIcon load={load} />
+              <Text style={{ textAlign: 'center' }}>{errorMessage}</Text>
+              <StatusBar style="auto" />
+          </View>
+      )
+  } else {
+      return (
+          <View style={styles.container}>
+              <ActivityIndicator size="large" color={colors.PRIMARY_COLOR} />
+              <StatusBar style="auto" />
+          </View>
+      )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#69f0ae',
-    flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#e7f7fe',
+      flex: 1,
+      justifyContent: 'center',
   },
   main: {
-    backgroundColor: '#69f0ae',
-    justifyContent: 'center',
-    flex: 1
-  }
-
-});
+      backgroundColor: '#70d9e7',
+      justifyContent: 'center',
+      flex: 1,
+  },
+})
